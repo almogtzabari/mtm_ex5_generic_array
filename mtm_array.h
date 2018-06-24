535eb92f31def596845e7b73382126b117184c09
almogtzabari <almogtzabari@campus.technion.ac.il>
2018-06-24 12:39:59 +0300

    updates

--- mtm_array.h
+++ mtm_array.h
@@ -7,7 +7,52 @@
 template <class T, int SIZE>
 class Array
 {
+    T* data;
+    int size;
 public:
+    /** Constructor */
+    explicit Array():size(SIZE),data(new T[SIZE]){}; // todo: throw exception if needed.
+
+    /** Copy constructor */
+    Array(const Array& copy_from){
+        size = copy_from.size;
+        for(int i=0;i<size;i++){
+            data[i] = copy_from.data[i]; // Requires operator= for <T>.
+        }
+    }
+
+    /** Operator= */
+    Array& operator=(const Array& array){
+        if(this == &array){
+            /* Self assignment. */
+            return *this;
+        }
+        delete[] data;
+        size = array.size;
+        data = new T[size];
+        for(int i=0; i<size; i++){
+            data[i] = array.data[i]; // Requires operator= for <T>.
+        }
+        return *this;
+    }
+
+    /** Operator[] */
+    T& operator[](const int i){
+        if(i<0 || i>=size){
+            throw std::out_of_range("mtm_array::operator []: index out of range!");
+        }
+        return data[i];
+    }
+
+    /** Const operator[] */
+    const T& operator[](const int i)const {
+        if(i<0 || i>=size){
+            throw std::out_of_range("mtm_array::operator []: index out of range!");
+        }
+        return data[i];
+    }
+
+
     // TODO: Need to complete interface of iterator classes (x++,++x, ->, operator==, operator!=), as well
     // as that of array itself (operator [], operator [] const, ctor, copy ctor, dtor, operator=),
     // and implement everything.
@@ -22,7 +67,9 @@ public:
         T const& operator*() const;
     };
 
-    iterator begin();
+    iterator begin(){
+        return data;
+    }
     const_iterator begin() const;
     iterator end();
     const_iterator end() const;
